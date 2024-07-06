e34e8c3b5a9fc0aa92e6a3ca1d20d4f325b18e8a
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2024-07-06 23:26:47 +0800

    feat: Use webpack for main process.
    This could be considered a perlimiary works for future dev on main process.

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
-module.exports = {
+
+const rendererProcessConfig = {
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
@@ -14,10 +15,10 @@ module.exports = {
     entry: "./src/renderer.jsx",
     output: {
         path: path.resolve(__dirname, "dist"),
-        library: {
-            type: 'module',
-        },
         filename: 'renderer.js',
+        library: {
+            type: 'module', // necessary in order to work with liteloader.
+        },
     },
     module: {
         rules: [
@@ -52,3 +53,60 @@ module.exports = {
         ],
     },
 };
+
+const mainProcessConfig = {
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+        // Explicitly resolve files with following extension as modules.
+        extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
+    },
+    experiments: {
+        outputModule: true,
+    },
+    target: 'electron-main',
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'main.js',
+        library: {
+            type: 'commonjs', // necessary in order to work with liteloader.
+        },
+        chunkFormat: 'module', // or 'module'
+    },
+    module: {
+        rules: [
+            {
+                test: /.(js|jsx)$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-react',
+
+                        ],
+                    }
+                },
+            },
+            {
+                test: /.(ts|tsx)$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-typescript',
+                            '@babel/preset-react',
+                        ],
+                    }
+                },
+            },
+        ],
+    },
+};
+
+module.exports = [rendererProcessConfig, mainProcessConfig];
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,19 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
-    watch: true,
-    mode: 'development',
-    devtool: 'inline-source-map',
+
+// module.exports = merge(common, {
+//     watch: true,
+//     mode: 'development',
+//     devtool: 'inline-source-map',
+// });
+
+const configs = common.map(function (config) {
+    return merge(config, {
+        watch: true,
+        mode: 'development',
+        devtool: 'inline-source-map',
+    });
 });
+
+module.exports = configs;
