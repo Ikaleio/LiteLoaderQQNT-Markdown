18cdc833b22d8c735b6259b75a94c55af2ce6ef2
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2024-07-13 00:32:36 +0800

    feat: Escaped removed dirty HTML instead of removal.
    When dirty node found inside HTML, escaped to show the innerHTML of the element as pure text instead of just remove it sliently.

---
+++ src/utils/htmlProc.ts
@@ -0,0 +1,53 @@
+// Utils function about HTML string process
+
+import { mditLogger } from "./logger";
+
+const DOMPurify = require('dompurify');
+
+DOMPurify.addHook('uponSanitizeElement', function (node: HTMLElement, data: any) {
+    // mditLogger('debug', 'PurifyHook', 'Data', data);
+    if (data.allowedTags[data.tagName] === true) {
+        // mditLogger('debug', 'PurifyHook', 'Hook skipped');
+        return;
+    }
+    let newNode = document.createElement('p');
+    newNode.innerText = node.outerHTML;
+    // mditLogger('debug', 'PurifyHook', 'New node', newNode);
+    node.replaceWith(newNode);
+});
+
+interface UponSanitizeDataRecv {
+    tagName: string;
+    allowedTags: Record<string, boolean>;
+}
+
+/**
+ * Unescape HTML entities in HTML string. Already unescaped HTML tag string will be ignored and not shown 
+ * in return string.
+ * @param {string} input 
+ * @returns {string} String with all HTML entities unescaped
+ */
+export function unescapeHtml(input: string) {
+    var doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+}
+
+export function escapeHtml(input: string) {
+    return input
+        .replaceAll('&', '&amp;')
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+        .replaceAll('"', '&quot;')
+        .replaceAll("'", '&#039;');
+}
+
+/**
+ * Using DOMPurify to purify HTML
+ * @param {string} input 
+ * @return {string} Purified HTML string.
+ */
+export function purifyHtml(input: string) {
+    let res = DOMPurify.sanitize(input);
+    mditLogger('debug', 'Purify', 'Removed', DOMPurify.removed);
+    return res;
+}
